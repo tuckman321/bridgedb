71d46bfb520c4c1316ee63197c98436e197fcfda
Tucker Raney <tuckerraney321@gmail.com>
2015-05-26 20:41:26 -0500

    edited when sign up is displayed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # note to set to actual host name
+  config.action_mailer.default_url_options = { host: 'bridgedb.herokuapp.com'}
 end
